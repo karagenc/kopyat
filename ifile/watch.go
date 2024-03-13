4223fb85df85b31e350c7ef0e67e8ed59d7af569
tomruk <41700170+tomruk@users.noreply.github.com>
2024-03-13 17:06:15 +0300

    Add status to watcher

--- ifile/watch.go
+++ ifile/watch.go
@@ -4,62 +4,113 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync/atomic"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"go.uber.org/zap"
 )
 
-type WatchJob struct {
-	log     *zap.Logger
-	stopped chan struct{}
+type (
+	WatchJob struct {
+		log     *zap.Logger
+		status  atomic.Int32
+		stopped chan struct{}
 
-	scanPath string
-	ifile    string
-}
+		scanPath string
+		ifile    string
+	}
+
+	Status int32
+)
+
+const (
+	StatusWillRun Status = iota
+	StatusRunning
+	StatusFailed
+	StatusStopped
+)
+
+const failAfter = 20 // 20 seconds
 
 func NewWatchJob(log *zap.Logger, ScanPath, Ifile string) *WatchJob {
-	return &WatchJob{
+	j := &WatchJob{
 		log:      log,
+		status:   atomic.Int32{},
 		stopped:  make(chan struct{}),
 		scanPath: ScanPath,
 		ifile:    Ifile,
 	}
+	j.status.Store(int32(StatusWillRun))
+	return j
 }
 
 func (j *WatchJob) ScanPath() string { return j.scanPath }
 
 func (j *WatchJob) Ifile() string { return j.ifile }
 
+func (j *WatchJob) Status() Status { return Status(j.status.Load()) }
+
 func (j *WatchJob) Start() error {
 	go func() {
 		err := j.walk()
 		if err != nil {
-			j.log.Error(err.Error())
+			j.logError(err)
+			j.fail()
 			return
 		}
 
-		watcher, c, err := watch(j.scanPath)
-		if err != nil {
-			j.log.Error(err.Error())
-			return
-		}
+		firstAttempt := time.Now()
 
+	outer:
 		for {
-			select {
-			case <-c:
-				err := j.walk()
-				if err != nil {
-					j.log.Error(err.Error())
+			watcher, c, err := watch(j.scanPath)
+			if err != nil {
+				j.logError(err)
+				j.sleepBeforeRetry(1)
+				if time.Since(firstAttempt).Seconds() >= failAfter {
+					j.fail()
+				}
+				continue
+			}
+
+			j.status.Store(int32(StatusRunning))
+
+			for {
+				select {
+				case <-c:
+					err := j.walk()
+					if err != nil {
+						j.logError(err)
+						j.sleepBeforeRetry(1)
+						if time.Since(firstAttempt).Seconds() >= failAfter {
+							j.fail()
+						}
+						continue outer
+					}
+				case <-j.stopped:
+					watcher.Close()
 					return
 				}
-			case <-j.stopped:
-				watcher.Close()
-				return
 			}
 		}
 	}()
 	return nil
 }
+
+func (j *WatchJob) logError(err error) {
+	if err != nil {
+		err = fmt.Errorf("watch: %v", err)
+		j.log.Error(err.Error())
+	}
+}
+
+func (j *WatchJob) sleepBeforeRetry(seconds time.Duration) {
+	time.Sleep(seconds * time.Second)
+	j.log.Sugar().Info("retry in %d second(s)", seconds)
+}
+
+func (j *WatchJob) fail() { j.status.Store(int32(StatusFailed)) }
 
 func (j *WatchJob) walk() error {
 	// TODO: Arguments
@@ -72,6 +123,8 @@ func (j *WatchJob) walk() error {
 }
 
 func (j *WatchJob) Stop() error {
+	close(j.stopped)
+	j.status.Store(int32(StatusStopped))
 	return nil
 }
 
