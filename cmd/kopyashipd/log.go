80fd99b53f3e35379fb832aaf38ee5ddce30d44e
tomruk <41700170+tomruk@users.noreply.github.com>
2024-03-14 13:44:08 +0300

    Add `removeNewLine`

--- cmd/kopyashipd/log.go
+++ cmd/kopyashipd/log.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"go.uber.org/zap"
 )
 
@@ -11,20 +13,33 @@ type logger struct {
 
 func newLogger(z *zap.Logger) *logger { return &logger{z: z, s: z.Sugar()} }
 
+func (l *logger) removeNewLine(s *string) {
+	*s = strings.ReplaceAll(*s, "\n", "")
+}
+
 func (l *logger) Info(a ...any) { l.s.Info(a...) }
 
-func (l *logger) Infof(format string, a ...any) { l.s.Infof(format, a...) }
+func (l *logger) Infof(format string, a ...any) {
+	l.removeNewLine(&format)
+	l.s.Infof(format, a...)
+}
 
 func (l *logger) Infoln(a ...any) { l.s.Info(a...) }
 
 func (l *logger) Warn(a ...any) { l.s.Warn(a...) }
 
-func (l *logger) Warnf(format string, a ...any) { l.s.Warnf(format, a...) }
+func (l *logger) Warnf(format string, a ...any) {
+	l.removeNewLine(&format)
+	l.s.Warnf(format, a...)
+}
 
 func (l *logger) Warnln(a ...any) { l.s.Warn(a...) }
 
 func (l *logger) Error(a ...any) { l.s.Error(a...) }
 
-func (l *logger) Errorf(format string, a ...any) { l.s.Errorf(format, a...) }
+func (l *logger) Errorf(format string, a ...any) {
+	l.removeNewLine(&format)
+	l.s.Errorf(format, a...)
+}
 
 func (l *logger) Errorln(a ...any) { l.s.Error(a...) }
